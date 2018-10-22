fc936835f0239e33226f45644fa7ea53031a0517
gilmar <jiraspiom@yahoo.com.br>
2018-10-22 20:10:13 -0300

    Initial Commit

---
+++ Alto Layout/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Alto Layout
+//
+//  Created by Gilmar Borges on 22/10/18.
+//  Copyright © 2018 alalgi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
